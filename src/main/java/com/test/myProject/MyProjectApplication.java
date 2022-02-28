ec669b7aa31fc9e94295c8a20a8ad33442f07cd6
chandu chandu <praneethas786@gmail.com>
2022-02-28 21:47:52 +0530

    commit 3

--- src/main/java/com/test/myProject/MyProjectApplication.java
+++ src/main/java/com/test/myProject/MyProjectApplication.java
@@ -9,6 +9,6 @@ public class MyProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
-
+//   commit changes
 //	changes made
 }
