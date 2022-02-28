31d285e4df47aadc1362f98946b478fb4414cf1f
chandu chandu <praneethas786@gmail.com>
2022-02-28 20:38:54 +0530

    first commit

---
+++ src/test/java/com/test/myProject/MyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.myProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
